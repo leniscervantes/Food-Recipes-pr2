eee3381a8d7a59db07b7f2a3f8d5391434ed0929
Lenis Cervantes <leniserik@gmail.com>
2022-09-13 13:10:51 +0200

    extraccion de datos de la api completada

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 const AxiosSp = require('../connect/axios.connect');
-const axiosCharacter = new AxiosSp();
+const axiosRecipe = new AxiosSp();
 
 
 
 router.get('/search-recipes', (req, res, next) => {
-  
-      res.render("recipes/search-recipes")
+  res.render("recipes/search-recipes")
     })
 
 
     router.post("/search-recipes", (req, res, next) => {
         const {ingredient, cuisine, diet, intolerances} = req.body
-        axiosCharacter
+        axiosRecipe
         .getRecipes(ingredient,cuisine, diet, intolerances)
         .then((recipes) => {
-          res.json(recipes)
-        //   res.redirect("/results")
-    })
+          res.render("recipes/result-recipes", recipes)
+        
+      })
+      .catch((err) => next(err))
 })
 
 module.exports = router;
